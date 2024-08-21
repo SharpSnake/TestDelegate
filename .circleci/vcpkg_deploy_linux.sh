d2994ca79c816f0107fb870422b81fcf15d0c667
Meteora <losengarden@aliyun.com>
2024-08-21 09:35:39 +0800

    update circleci

---
+++ .circleci/vcpkg_deploy_linux.sh
@@ -0,0 +1,7 @@
+# Run the following cmds before use of g++ each time
+
+export VCPKG_ROOT=$PWD/vcpkg
+export LIBRARY_PATH=$VCPKG_ROOT/installed/x64-linux/lib:\$LIBRARY_PATH
+export LD_LIBRARY_PATH=$VCPKG_ROOT/installed/x64-linux/lib:\$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$VCPKG_ROOT/installed/x64-linux/include:\$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$VCPKG_ROOT/installed/x64-linux/include:\$CPLUS_INCLUDE_PATH
